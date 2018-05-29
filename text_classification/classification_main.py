22602374ae506bfcc388ee70b3f267b27ab942e2
joan-wang <joanyiqunwang@gmail.com>
2018-05-28 23:22:36 -0500

    forgot to commit these

--- text_classification/classification_main.py
+++ text_classification/classification_main.py
@@ -11,6 +11,8 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import GaussianNB, MultinomialNB
 from sklearn.tree import DecisionTreeClassifier
 
+# define columns to use as features
+FEATURE_COLS = ['words']#, 'agency_pos_prop','power_pos_prop', 'agency_neg_prop','power_neg_prop']
 
 # big loop
 CLASSIFIERS = {'RF': RandomForestClassifier(),
@@ -35,62 +37,71 @@ GRID_BEST = {'MNB': {'alpha':[0.5]}, 'LR': {'penalty': ['l1', 'l2'], 'C': [1]}}
 CLASSIFIER_FINAL = {'MNB': MultinomialNB()}
 GRID_FINAL = {'MNB': {'alpha':[0.5]}}
 
+def train_model(input_name, output_name, feature_cols, classifier_dict, grid_dict, genre = 'all', speaker_pairs = False):
+    """
+    input_name: pickle file containing preprocessed dataframe, before feature generation
+    output_name: csv file to write results
+    """
+    X_train, X_test, y_train, y_test, count_vect, tfidf_transformer = pipeline.prepare_data(input_name, feature_cols, genre=genre, speaker_pairs = speaker_pairs)
+    results, classifier_objects = pipeline.fit_models(X_train, X_test, y_train, y_test, CLASSIFIERS_BEST, GRID_BEST, count_vect, tfidf_transformer) 
+    results.to_csv(output_name)
+    
+    return results, classifier_objects
+
+
+results, classifier_objects = train_model("../data/movies_lines_train.p", "results_lr_mnb.csv", FEATURE_COLS,CLASSIFIERS_BEST, GRID_BEST)
+pickle.dump(classifier_objects[0], open('mnb_final.p', 'wb'))
+
+'''
+OLD CODE THAT WAS PREVIOUSLY RUN
 ###################################
 
 # Ran the code below multiple times for different classifiers groups
 # Logistic regression and multinomial NB do the best at 71% accuracy
 # But recall is really bad in all these models! Not capturing most 
-X_train, X_test, y_train, y_test, count_vect, tfidf_transformer = pipeline.prepare_data("../data/movies_features.p")
-results, classifer_objects = pipeline.fit_models(X_train, X_test, y_train, y_test, CLASSIFIERS, GRID, count_vect, tfidf_transformer) 
-results.to_csv('results_mnb.csv')
+X_train, X_test, y_train, y_test, count_vect, tfidf_transformer = pipeline.prepare_data("../data/movies_lines_train_features.p", FEATURE_COLS)
+results, classifier_objects = pipeline.fit_models(X_train, X_test, y_train, y_test, CLASSIFIERS_BEST, GRID_BEST, count_vect, tfidf_transformer) 
+results.to_csv('results_lr_mnb.csv')
 
 ###################################
-
 # Create separate models by genre
 # Romance gets accuracy of 80%
-X_train, X_test, y_train, y_test,count_vect, tfidf_transformer = pipeline.prepare_data("../data/movies_features.p", genre = 'romance')
-results, classifer_objects = pipeline.fit_models(X_train, X_test, y_train, y_test, CLASSIFIERS_BEST, GRID_BEST, count_vect, tfidf_transformer)
+X_train, X_test, y_train, y_test,count_vect, tfidf_transformer = pipeline.prepare_data("../data/movies_features.p", FEATURE_COLS, genre = 'romance')
+results, classifier_objects = pipeline.fit_models(X_train, X_test, y_train, y_test, CLASSIFIERS_BEST, GRID_BEST, count_vect, tfidf_transformer)
 results.to_csv('results_romance.csv')
 
 # Action gets accuracy of 70%
-X_train, X_test, y_train, y_test, count_vect, tfidf_transformer = pipeline.prepare_data("../data/movies_features.p", genre = 'action')
-results, classifer_objects = pipeline.fit_models(X_train, X_test, y_train, y_test, CLASSIFIERS_BEST, GRID_BEST, count_vect, tfidf_transformer)
+X_train, X_test, y_train, y_test, count_vect, tfidf_transformer = pipeline.prepare_data("../data/movies_features.p", FEATURE_COLS, genre = 'action')
+results, classifier_objects = pipeline.fit_models(X_train, X_test, y_train, y_test, CLASSIFIERS_BEST, GRID_BEST, count_vect, tfidf_transformer)
 results.to_csv('results_action.csv')
 
 # Drama gets accuracy of 69%
-X_train, X_test, y_train, y_test, count_vect, tfidf_transformer = pipeline.prepare_data("../data/movies_features.p", genre = 'drama')
-results, classifer_objects = pipeline.fit_models(X_train, X_test, y_train, y_test, CLASSIFIERS_BEST, GRID_BEST, count_vect, tfidf_transformer)
+X_train, X_test, y_train, y_test, count_vect, tfidf_transformer = pipeline.prepare_data("../data/movies_features.p", FEATURE_COLS, genre = 'drama')
+results, classifier_objects = pipeline.fit_models(X_train, X_test, y_train, y_test, CLASSIFIERS_BEST, GRID_BEST, count_vect, tfidf_transformer)
 results.to_csv('results_drama.csv')
 
 # Comedy gets accuracy of 65%
-X_train, X_test, y_train, y_test, count_vect, tfidf_transformer = pipeline.prepare_data("../data/movies_features.p", genre = 'comedy')
-results,classifer_objects = pipeline.fit_models(X_train, X_test, y_train, y_test, CLASSIFIERS_BEST, GRID_BEST, count_vect, tfidf_transformer, )
+X_train, X_test, y_train, y_test, count_vect, tfidf_transformer = pipeline.prepare_data("../data/movies_features.p", FEATURE_COLS, genre = 'comedy')
+results,classifier_objects = pipeline.fit_models(X_train, X_test, y_train, y_test, CLASSIFIERS_BEST, GRID_BEST, count_vect, tfidf_transformer, )
 results.to_csv('results_comedy.csv')
 
 # Crime gets accuracy of 69%
-X_train, X_test, y_train, y_test, count_vect, tfidf_transformer = pipeline.prepare_data("../data/movies_features.p", genre = 'crime')
-results, classifer_objects = pipeline.fit_models(X_train, X_test, y_train, y_test, CLASSIFIERS_BEST, GRID_BEST, count_vect, tfidf_transformer, )
+X_train, X_test, y_train, y_test, count_vect, tfidf_transformer = pipeline.prepare_data("../data/movies_features.p", FEATURE_COLS, genre = 'crime')
+results, classifier_objects = pipeline.fit_models(X_train, X_test, y_train, y_test, CLASSIFIERS_BEST, GRID_BEST, count_vect, tfidf_transformer, )
 results.to_csv('results_crime.csv')
 
 
 ###################################
 # Clasisfy by speaker pair - probably not useful for this project
 # Accuracy is 47%. 
-X_train, X_test, y_train, y_test, count_vect, tfidf_transformer = pipeline.prepare_data("../data/movies_features.p", speaker_pairs = True)
-results, classifer_objects = pipeline.fit_models(X_train, X_test, y_train, y_test, CLASSIFIERS_BEST, GRID_BEST, count_vect, tfidf_transformer, multiclass = True)
+X_train, X_test, y_train, y_test, count_vect, tfidf_transformer = pipeline.prepare_data("../data/movies_features.p", FEATURE_COLS, speaker_pairs = True)
+results, classifier_objects = pipeline.fit_models(X_train, X_test, y_train, y_test, CLASSIFIERS_BEST, GRID_BEST, count_vect, tfidf_transformer, multiclass = True)
 results.to_csv('results_speaker_pairs.csv')
 
 
 ###################################
 # Save the final model as a pickle file
-X_train, X_test, y_train, y_test, count_vect, tfidf_transformer = pipeline.prepare_data("../data/movies_features.p")
-results, classifer_objects = pipeline.fit_models(X_train, X_test, y_train, y_test, CLASSIFIER_FINAL, GRID_FINAL, count_vect, tfidf_transformer)
-pickle.dump(classifer_objects[0], open('mnb_final.p', 'wb'))
-
-
-
-'''
-clf = MultinomialNB(alpha = 0.5)
-clf_fitted = clf.fit(X_train, y_train)
-pickle.dump(clf_fitted, open('mnb.p', 'wb'))
+X_train, X_test, y_train, y_test, count_vect, tfidf_transformer = pipeline.prepare_data("../data/movies_features.p", FEATURE_COLS)
+results, classifier_objects = pipeline.fit_models(X_train, X_test, y_train, y_test, CLASSIFIER_FINAL, GRID_FINAL, count_vect, tfidf_transformer)
+pickle.dump(classifier_objects[0], open('mnb_final.p', 'wb'))
 '''
--- text_classification/classification_pipeline.py
+++ text_classification/classification_pipeline.py
@@ -17,6 +17,7 @@ from sklearn.naive_bayes import GaussianNB, MultinomialNB
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer,TfidfTransformer
+from scipy import sparse
 
 
 
@@ -30,53 +31,98 @@ class TextClassifier():
         self.tfidf_transformer = tfidf_transformer
         
 
-def prepare_data(filename, genre = 'all', speaker_pairs = False, random_state = 42):
+def prepare_data(filename, feature_cols, genre = 'all', speaker_pairs = False, random_state = 42):
     '''
     Prepare data for modeling. Get the same train/test split unless random_state is specified.
     
     filename: path to pickled dataframe
-    '''
-    # load pre-processed df
-    df = pickle.load(open(filename, 'rb'))
-   
+    ''' 
+    df = generate_features(filename)
+    
     # specify genre
     if genre != 'all':
         df = df[df.genre == genre]
     
-    # split into train and test sets
+    # split into train and validation sets
     if speaker_pairs: # use gender pairing as outcome classes
-        X_train, X_test, y_train, y_test = train_test_split(df.words, df.gender_pair.astype('int'), test_size=0.33, random_state=random_state)
+        X_train, X_test, y_train, y_test = train_test_split(df[feature_cols], df.gender_pair.astype('int'), test_size=0.33, random_state=random_state)
     else: # use gender_from (M/F) as outcome classes
-        X_train, X_test, y_train, y_test = train_test_split(df.words, df.gender_from.astype('int'), test_size=0.33, random_state=random_state)
+        X_train, X_test, y_train, y_test = train_test_split(df[feature_cols], df.gender_from.astype('int'), test_size=0.33, random_state=random_state)
 
     # transform train and test sets
-    X_train_tfidf, count_vect, tfidf_transformer = transform_train(X_train)
-    X_test_tfidf = transform_test(X_test, count_vect, tfidf_transformer)
+    X_train_tfidf_combined, count_vect, tfidf_transformer = transform_train(X_train)
+    X_test_tfidf_combined = transform_test(X_test, count_vect, tfidf_transformer)
 
-    return X_train_tfidf, X_test_tfidf, y_train, y_test, count_vect, tfidf_transformer
+    return X_train_tfidf_combined, X_test_tfidf_combined, y_train, y_test, count_vect, tfidf_transformer
+
+
+def generate_features(filename):
+    # load pre-processed df on which to add features
+    df = pickle.load(open(filename, 'rb'))
+    
+    # Treat gender_from and gender_to columns: remove unknown gender, 0 is male and 1 is female
+    df = df[df.gender_from != '?']
+    df['gender_from'] = np.where(df.gender_from == 'f', 1, 0)
+    
+    # Treat gender_to column: remove unknown gender, 0 is male and 1 is female
+    df = df[df.gender_to != '?']
+    df['gender_to'] = np.where(df.gender_to == 'f', 1, 0)
+        
+    # Create gender pair column
+    # MM = 0, MF = 1, FM = 2, FF = 3
+    conditions = [(df.gender_from == 0) & (df.gender_to == 0),
+         (df.gender_from == 0) & (df.gender_to == 1),
+         (df.gender_from == 1) & (df.gender_to == 0),
+         (df.gender_from == 1) & (df.gender_to == 1)]
+    choices = [0,1,2,3]
+    df['gender_pair'] = np.select(conditions, choices)
+    
+    print(df.shape)
+    
+    # load df with power/agency verb columns and join with main
+    df_verbs = pickle.load(open("../data/movies_verbs.p", 'rb'))
+    df_verbs.fillna(0, inplace=True)
+    df = pd.merge(df, 
+                  df_verbs[['line_id', 'agency_pos_prop','power_pos_prop', 'agency_neg_prop','power_neg_prop']], 
+                  how='left', 
+                  on='line_id')
+    
+    print(df.shape)
+    
+    return df
 
 
 def transform_train(X_train):
     count_vect = CountVectorizer() # using bag of words
     tfidf_transformer = TfidfTransformer()
-
-    X_train_counts = count_vect.fit_transform(X_train)    
+    
+    # vectorize the text column
+    X_train_counts = count_vect.fit_transform(X_train.words)    
     X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
     
-    return X_train_tfidf, count_vect, tfidf_transformer
+    # combine sparse matrix of text features with other features
+    X_train_others = sparse.csr_matrix(X_train.loc[:, X_train.columns != 'words'])
+    X_train_tfidf_combined = sparse.hstack((X_train_tfidf, X_train_others)).tocsr()
+    
+    return X_train_tfidf_combined, count_vect, tfidf_transformer
 
 
 def transform_test(X_test, count_vect, tfidf_transformer):
-    X_test_counts = count_vect.transform(X_test) # JW TO DO: how does this treat unseen ngrams?
+    X_test_counts = count_vect.transform(X_test.words)
     X_test_tfidf = tfidf_transformer.transform(X_test_counts)
     
-    return X_test_tfidf
+    # combine sparse matrix of text features with other features
+    X_test_others = sparse.csr_matrix(X_test.loc[:, X_test.columns != 'words'])
+    X_test_tfidf_combined = sparse.hstack((X_test_tfidf, X_test_others)).tocsr()
+    
+    return X_test_tfidf_combined
 
 
 def fit_models(X_train, X_test, y_train, y_test, CLASSIFIERS, GRID, count_vect, tfidf_transformer, multiclass = False):
     """
     Loop through a number of classification fit_models and parameters, saving results for each run
     Based off code from previous homework assignment: https://github.com/joan-wang/Machine-Learning/blob/master/hw3/model.py
+    If speaker_pairs = True when prepare_data function was called, then multiclass must also be True.
     """
     classifier_objects = []
     results =  pd.DataFrame(columns=('model_type','clf', 'parameters', 'auc-roc', 
